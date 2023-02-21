ee8a901de8689514bd39de4564c7aa50b1988311
juan-gestionix <juan.hernandez@gestionix.com>
2023-02-21 16:26:27 -0600

    Proyecto examen digitek

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes ,Route, BrowserRouter as Router}  from 'react-router-dom'
+import Notebook from './pages/Notebook';
+import Welcome from './pages/Welcome';
+
+function App() {
+ 
+  return (
+    <Router>
+        <Routes>
+        <Route path="/" element={<Welcome/>}/>
+        <Route path="/notebook" element={ <Notebook/>}/>
+      </Routes>
+    </Router>
+   
+  );
+}
+
+export default App;
